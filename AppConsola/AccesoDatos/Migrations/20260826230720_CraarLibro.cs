e0882395b10687a90f4817ba273cd40e9d262764
Martincagliero <martincagliero06@gmail.com>
2026-08-26 20:17:30 -0300

    Adjunto ej

---
+++ AppConsola/AccesoDatos/Migrations/20260826230720_CraarLibro.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Libro_Libro_LibroId",
+                table: "Libro");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Libro_LibroId",
+                table: "Libro");
+
+            migrationBuilder.DropColumn(
+                name: "LibroId",
+                table: "Libro");
+
+            migrationBuilder.RenameColumn(
+                name: "Año",
+                table: "Libro",
+                newName: "Anio");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libro_AutorId",
+                table: "Libro",
+                column: "AutorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Libro_Autor_AutorId",
+                table: "Libro",
+                column: "AutorId",
+                principalTable: "Autor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Libro_Autor_AutorId",
+                table: "Libro");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Libro_AutorId",
+                table: "Libro");
+
+            migrationBuilder.RenameColumn(
+                name: "Anio",
+                table: "Libro",
+                newName: "Año");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LibroId",
+                table: "Libro",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libro_LibroId",
+                table: "Libro",
+                column: "LibroId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Libro_Libro_LibroId",
+                table: "Libro",
+                column: "LibroId",
+                principalTable: "Libro",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ AppConsola/AccesoDatos/Models/Autor.cs
@@ -0,0 +1,12 @@
+﻿namespace AccesoDatos.Models
+{
+    public class Autor
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Libro> Libros { get; set; } = new();
+
+    }
+}
